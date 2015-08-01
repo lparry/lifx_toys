0c28c5f49ecc80a033c06acbbebb2511aa13a71c
Lucas Parry <lparry@gmail.com>
2015-08-01 18:16:31 +1000

    Random colors

---
+++ colors.rb
@@ -0,0 +1,33 @@
+require_relative "lifx_light"
+
+class Colors
+
+  HUE_RANGE = 0..360
+  BRIGHTNESS_RANGE = 20..80
+  SATURATION_RANGE = 20..100
+
+  CYCLE_TIME = 10.0
+
+  def initialize
+    @lights = LifxLight.get_lights.select{|x| x.connected }
+  end
+
+  def run
+    while (true)
+      @lights.each do |light|
+        light.set_color(random_color, random_brightness, CYCLE_TIME* 1.5)
+        sleep(CYCLE_TIME/ @lights.count)
+      end
+    end
+  end
+
+  def random_color
+    "hue:#{rand(HUE_RANGE)} saturation:#{rand(SATURATION_RANGE).to_f / 100}"
+  end
+
+  def random_brightness
+    rand(BRIGHTNESS_RANGE).to_f / 100
+  end
+end
+
+Colors.new.run
