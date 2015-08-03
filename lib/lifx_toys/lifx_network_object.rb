47170feefcf6c814ba98b5d32a8cf718115b1ecf
Lucas Parry <lparry@gmail.com>
2015-08-03 13:48:11 +1000

    This class was getting ugly

--- lib/lifx_toys/lifx_network_object.rb
+++ lib/lifx_toys/lifx_network_object.rb
@@ -11,23 +11,23 @@ module LifxToys
     end
 
     def get_info
-      HttpApi.get_light_info(selector)
+      make_request :get_light_info
     end
 
     def turn_on(options = {})
-      HttpApi.set_power_state(selector, 'on', options)
+      make_request :set_power_state, 'on', options
     end
 
     def toggle
-      HttpApi.toggle_power_state(selector)
+      make_request :toggle_power_state
     end
 
     def turn_off(options = {})
-      HttpApi.set_power_state(selector, 'off', options)
+      make_request :set_power_state, 'off', options
     end
 
     def set_color(color, options = {})
-      response = HttpApi.set_color(selector, color, options)
+      response = make_request :set_color, color, options
       if response.success?
         puts "info: set #{selector} to #{color}" if ENV["DEBUG"]
         nil
@@ -35,6 +35,10 @@ module LifxToys
         puts "warning: light status - #{response}"
         -1
       end
+    end
+
+    def make_request(method_name, *args)
+      HttpApi.public_send(method_name, selector, *args)
     end
   end
 end
