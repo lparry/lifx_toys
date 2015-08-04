ff010d3857a6400f722e890c83b38b6e50b97d5a
Lucas Parry <lparry@gmail.com>
2015-08-04 23:45:14 +1000

    Random colors using LAN API

--- lib/lifx_toys/colors.rb
+++ lib/lifx_toys/colors.rb
@@ -1,25 +1,42 @@
-require "lifx_http"
+require "lifx"
 
 module LifxToys
   class Colors
 
     HUE_RANGE = 0..360
     BRIGHTNESS_RANGE = 20..80
-    SATURATION_RANGE = 20..100
+    SATURATION_RANGE = 40..100
 
     CYCLE_TIME = 10.0
 
+    attr_reader :client
+
     def initialize
-      @lights = LifxHttp::LifxLight.get_lights.select{|x| x.connected }
+      @client = LIFX::Client.lan
+
+      client.discover!
+    end
+
+    def lights
+      client.lights
+    end
+
+    def light_count
+      lights.count
     end
 
     def run
       while (true)
-        @lights.each do |light|
-          light.set_color("hue:#{random_hue} saturation:#{random_saturation} brightness:#{random_brightness}", duration: CYCLE_TIME* 1.5)
-          sleep(CYCLE_TIME/ @lights.count)
+        lights.each do |light|
+          light.set_color(random_color, duration: CYCLE_TIME * 1.5)
+          sleep(CYCLE_TIME/ light_count)
         end
+        client.flush
       end
+    end
+
+    def random_color
+      LIFX::Color.hsb(random_hue, random_saturation, random_brightness)
     end
 
     def random_hue
